f2654f55e1a48d1ed657af2f57f73181c0ac59bc
ajithacc <ajith.a.s@accenture.com>
2026-06-22 15:50:34 +0530

    Initial Commit

---
+++ RetailBrainApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RetailBrainApp
+//
+//  Created by ajith.a.s on 22/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RetailBrainApp/RetailBrainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetailBrainAppApp.swift
+//  RetailBrainApp
+//
+//  Created by ajith.a.s on 22/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RetailBrainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
